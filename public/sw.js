7be173966b437438b9c3e987adbb75f7e498a2f5
beaconhillacademy123 <beaconhillacademykano@gmail.com>
2026-09-23 19:52:46 -0700

    Harden offline service worker fallback

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,14 @@
 const CACHE_NAME='m-king-reads-v3';
 const APP_SHELL=['/','/login','/manifest.webmanifest','/icon-192.svg','/icon-512.svg'];
 
+async function cacheResponse(request,response){
+  if(!response || !response.ok) return response;
+  const copy=response.clone();
+  const cache=await caches.open(CACHE_NAME);
+  await cache.put(request,copy);
+  return response;
+}
+
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)));
   self.skipWaiting();
@@ -27,13 +35,7 @@ self.addEventListener('fetch',event=>{
 
   event.respondWith(
     fetch(event.request)
-      .then(response=>{
-        if(response.ok){
-          const copy=response.clone();
-          caches.open(CACHE_NAME).then(cache=>cache.put(event.request,copy)).catch(()=>{});
-        }
-        return response;
-      })
-      .catch(()=>caches.match(event.request).then(response=>response || caches.match('/')))
+      .then(response=>cacheResponse(event.request,response).catch(()=>response))
+      .catch(()=>caches.match(event.request).then(response=>response || (isNavigation ? caches.match('/') : undefined)))
   );
 });
